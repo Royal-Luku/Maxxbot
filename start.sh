a35d133bd09329938f9a66e5e3e04b558d09669e
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-07-12 14:00:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Royaldeep1/Mdisk-Search-Bot-Pro /Mdisk-Search-Bot-Pro
-cd /Mdisk-Search-Bot-Pro
+git clone https://github.com/Royaldeep1/Maxxbot /Maxxbot
+cd /Maxxbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
