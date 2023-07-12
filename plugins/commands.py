8d5572c3330bef54038ecf9b8b110d62cc23db82
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-07-12 13:30:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,8 +38,8 @@ async def start_handler(_,event: Message):
             InlineKeyboardButton('➕ Add Me To Your Group ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ],
 
-             [InlineKeyboardButton("About", callback_data="About_msg"),
-             InlineKeyboardButton("Help", callback_data="Help_msg")
+             [InlineKeyboardButton("Group", url=f'https://t.me/+y2y-pg_jjGdiMTY1'),
+             InlineKeyboardButton("Owner", url=f'https://t.me/dreamer999')
              ]
         ])
     )
